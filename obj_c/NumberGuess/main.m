ee54892de4ead21b5e69caf12d11cabb130d487a
Allan Davis <cajun.code@gmail.com>
2012-04-04 23:32:00 -0400

    Adding ObjC Project to set

---
+++ obj_c/NumberGuess/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  NumberGuess
+//
+//  Created by Allan Davis on 4/4/12.
+//  Copyright (c) 2012 CajunCode. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
